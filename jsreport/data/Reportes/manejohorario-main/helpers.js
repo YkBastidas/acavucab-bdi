25bc1f88ff1f84e318221badb29c3ba5cd105e27
Mariosor12 <55864049+Mariosor12@users.noreply.github.com>
2020-01-17 00:06:53 -0400

    Add files via upload

---
+++ jsreport/data/Reportes/manejohorario-main/helpers.js
@@ -0,0 +1,9 @@
+function now() {
+    return new Date().toLocaleDateString()
+}
+
+function nowPlus20Days() {
+    var date = new Date()
+    date.setDate(date.getDate() + 20);
+    return date.toLocaleDateString();
+}
